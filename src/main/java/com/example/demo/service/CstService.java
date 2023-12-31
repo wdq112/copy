416afed43bab1d5624565c4ed60a4e20b665f1d3
wudaoqing <1920132572@qq.com>
2023-12-31 11:19:47 +0800

    first init

--- src/main/java/com/example/demo/service/CstService.java
+++ src/main/java/com/example/demo/service/CstService.java
@@ -1,20 +1,13 @@
 package com.example.demo.service;
 
-import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.thread.ExecutorBuilder;
 import cn.hutool.core.util.IdUtil;
-import cn.hutool.json.JSONUtil;
 import com.example.demo.entity.Item;
-import com.example.demo.entity.RangeId;
-import com.example.demo.entity.RangeList;
 import com.example.demo.repo.ItemRepo;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Range;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.JdbcClient;
@@ -22,9 +15,9 @@ import org.springframework.stereotype.Service;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
@@ -58,25 +51,24 @@ public class CstService {
         for (int i = 0; i < count / 5000 + 1; i++) {
             var d = jdbcClient.sql("select id from item where id>? order by id  limit 5000").param(minId).query(Long.class).list();
             log.info(String.valueOf(d.size()));
-            if (d.isEmpty()) {
+            List<Long> tmpList = jdbcClient.sql("select id from item where id>? order by id limit 5000").param(minId).query(Long.class).list();
+            if (tmpList.isEmpty()) {
                 break;
             }
-            long tmp = jdbcClient.sql("select id from item where id>? order by id limit 5000").param(minId).query(Long.class).list().getLast();
-            var rangeId = new RangeId(minId, Math.min(maxId, tmp + 1));
-            a.add(JSONUtil.toJsonStr(rangeId));
+            long tmp = tmpList.getLast();
+            a.add(minId + "%" + Math.min(maxId, tmp + 1));
             minId = tmp + 1;
         }
-        redisTemplate.opsForList().rightPushAll("copyList", a);
 //        Lists.partition()
     }
 
     public void copy() throws InterruptedException {
         var startTime = System.currentTimeMillis();
         prepareCopy();
-        log.info("prepare : {}s",(System.currentTimeMillis() - startTime) / 1000);
+        log.info("prepare : {}s", (System.currentTimeMillis() - startTime) / 1000);
         var tmp = redisTemplate.opsForList().range("copyList", 0, -1);
         assert tmp != null;
-        var rangeList = tmp.stream().map(it -> JSONUtil.toBean(it, RangeId.class)).toList();
+        var rangeList = tmp.stream().map(it -> it.split("%",2)).toList();
         CountDownLatch countDownLatch = new CountDownLatch(rangeList.size());
         for (int i = 0; i < rangeList.size(); i++) {
             int finalI = i;
@@ -84,7 +76,12 @@ public class CstService {
                 String sql = """
                         insert into item values(?,?,?,?);
                         """;
-                List<Item> itemList = jdbcClient.sql("select * from item where id>? and id<?").param(rangeList.get(finalI).getMinId()).param(rangeList.get(finalI).getMaxId()).query(Item.class).list();
+                var b = rangeList.get(finalI);
+                List<Item> itemList = jdbcClient.sql("select * from item where id>:startId and id<:endId")
+                        .param("startId", b[0], Types.BIGINT)
+                        .param("endId", b[1], Types.BIGINT)
+                        .query(Item.class)
+                        .list();
                 if (CollectionUtil.isEmpty(itemList)) {
                     countDownLatch.countDown();
                     return;
